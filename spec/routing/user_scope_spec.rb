88ad7da439f7decd1e1611f8a1bf7f7d316b2db6
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-11-05 14:17:45 +0900

    add routing spec

---
+++ spec/routing/user_scope_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe '/:user', type: :routing do
+  context 'resources :blog_contents' do
+    subject(:blog_entry) { { get: '/routing_user/blog/target_blog_id' } }
+    it 'routes to /:user/blog/:id' do
+      expect(blog_entry).to route_to(
+                              controller: 'blog_contents',
+                              action: 'show',
+                              user: 'routing_user',
+                              id: 'target_blog_id',
+                            )
+    end
+  end
+end

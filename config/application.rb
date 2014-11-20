6aa99fbad95d27b7deb700e58ebf86ac57085a4d
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-11-20 13:05:41 +0900

    add setting to precompile assets

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Neecomi
       Rails.root.join('lib/autoload'),
     ]
 
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

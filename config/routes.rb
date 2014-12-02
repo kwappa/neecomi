f2cbc5839e1814a75dfacf010f1dc3e3062d6712
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-02 13:09:07 +0900

    modify routes for blog_contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :blog_contents, path: :blog, as: :blog, param: :slug, only: [:create, :edit, :destroy]
+
   resources :users
   scope ':name' do
     get '/', controller: :users, action: :show
-    resources :blog_contents, path: :blog, as: :blog, param: :slug
+    resources :blog_contents, path: :blog, as: :blog, param: :slug, only: [:index, :show]
   end
 end

c3ef6bb69cc2ece1b1bb2a7a17403b5aa5f2411d
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-02 20:02:47 +0900

    remove duplicated routes setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :blog_contents, path: :blog, as: :blog, param: :slug, only: [:create, :edit, :destroy]
-
   devise_for :users
 
-  resources :users
+  resources :blog_contents, path: :blog, as: :blog, param: :slug, only: [:create, :edit, :destroy]
+
   scope ':name' do
     get '/', controller: :users, action: :show
     resources :blog_contents, path: :blog, as: :blog, param: :slug, only: [:index, :show]

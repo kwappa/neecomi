d1c261afa3288a357c7599112582b3ac491d0c9d
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-06 15:47:53 +0900

    add controller spec for users#sign_out

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  pending do
-    describe 'get #show' do
-      subject(:request) { get(:show, id: 1) }
-      context 'when user does not found' do
-        it { expect(request).to be_not_found }
-      end
+  let(:user) do
+    User.create!(
+      name: 'test_user',
+      email: 'test_user@example.com',
+      password: 'password'
+    )
+  end
+
+  describe 'get #sign_out' do
+    subject(:request) { get(:sign_out) }
+
+    context 'when user is logging in' do
+      before { sign_in user }
+      it { expect(request).to be_ok }
+    end
+
+    context 'when user does not logging in' do
+      it { expect(request).to redirect_to root_path }
     end
   end
 end

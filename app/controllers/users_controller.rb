9409d0c7e5507ac84b76a9281d471c132180addc
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-12-07 10:22:52 +0900

    specify redirect path after edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_user!, only: [:show, :after_edit]
 
   def sign_out
     if user_signed_in?
@@ -11,5 +11,9 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+  end
+
+  def after_edit
+    redirect_to "/#{current_user.name}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   devise_for :users
+
+  get 'user_root', controller: :users, action: :after_edit, method: :get
+
   namespace :users do
     get :sign_out
   end
-
 
   resources :blog_contents, path: :blog, as: :blog, param: :slug, only: [:create, :edit, :destroy]
 
